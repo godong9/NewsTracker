86db647084b8cb3ebb09301520f0fb3fb9538408
Donghyun Go <godong9@gmail.com>
2016-10-31 22:08:27 +0900

    Update crawler
    - Add 동아일보
    - Add 한겨레

--- news-tracker/crawler/crawler.js
+++ news-tracker/crawler/crawler.js
@@ -4,10 +4,13 @@ var fs = require('fs');
 
 var NEWS_JOINS_ROOT = 'http://news.joins.com';
 var NEWS_CHOSUN_ROOT = 'http://search.chosun.com';
+var NEWS_DONGA_ROOT = 'http://news.donga.com';
+var NEWS_HANI_ROOT = 'http://www.hani.co.kr';
 
 var c = new Crawler({
   maxConnections : 10,
   forceUTF8: true,
+  skipDuplicates: true,
   // This will be called for each crawled page
   callback : function (error, result, $) {
     // 중앙일보 이슈 태그에서 기사 URI 추출해서 큐에 추가
@@ -25,9 +28,33 @@ var c = new Crawler({
     // 조선일보 검색 페이지 태그에서 기사 페이지 URI 추출해서 큐에 추가
     $('section.news dl > dt > a').each(function(index, a) {
       var toQueueUri = $(a).attr('href');
-      console.log(toQueueUri);
       c.queue(toQueueUri);
     });
+
+    // 동아일보 기사 페이지에서 페이지 번호에 해당하는 URI 추출해서 큐에 추가
+    $('div.page > a').each(function(index, a) {
+      var toQueueUri = $(a).attr('href');
+      c.queue(result.uri + toQueueUri);
+    });
+
+    // 동아일보 기사 URI 추출해서 큐에 추가
+    $('div.articleList p.title a').each(function(index, a) {
+      var toQueueUri = $(a).attr('href');
+      c.queue(toQueueUri);
+    });
+
+    // 한겨레 기사 페이지에서 페이지 번호에 해당하는 URI 추출해서 큐에 추가
+    $('div.paginate > a').each(function(index, a) {
+      var toQueueUri = $(a).attr('href');
+      c.queue(NEWS_HANI_ROOT + toQueueUri);
+    });
+
+    // 한겨레 기사 URI 추출해서 큐에 추가
+    $('h4.article-title > a').each(function(index, a) {
+      var toQueueUri = $(a).attr('href');
+      c.queue(NEWS_HANI_ROOT + toQueueUri);
+    });
+
 
     // 기사 본문 내용
     $('body').each(function(index, tag) {
@@ -38,23 +65,14 @@ var c = new Crawler({
       getChosunArticleData($, tag);
       // 조선비즈 기사
       getChosunBizArticleData($, tag);
-      // TODO: 동아일보 기사
-      // TODO: 한겨레 기사
+      // 동아일보 기사
+      getNewsDongaArticleData($, tag);
+      // 한겨레 기사
+      getNewsHaniArticleData($, tag);
     });
 
   }
 });
-
-// 중앙일보 이슈 페이지 URI 가져옴
-function getNewsJoinsIssueUri(issue) {
-  return NEWS_JOINS_ROOT + '/issue/' + issue;
-}
-
-// 조선일보 기사 검색 페이지 URI 가져옴
-function getChosunUri(searchStr) {
-  return encodeURI(NEWS_CHOSUN_ROOT + '/search/news.search?query=' + searchStr + '&orderby=default');
-}
-
 
 // 중앙일보 기사 페이지에서 데이터 추출해서 DB에 저장
 function getNewsJoinsArticleData($, tag) {
@@ -89,17 +107,70 @@ function getChosunBizArticleData($, tag) {
   }
 }
 
+// 동아일보 기사 페이지에서 데이터 추출해서 DB에 저장
+function getNewsDongaArticleData($, tag) {
+  var title = $(tag).find('div.article_title02 > h1').text();
+  var content = $(tag).find('div.article_txt').text();
+  if (title.length > 0 && content.length > 0) {
+    // TODO: save to db
+    console.log(title);
+    //console.log(content);
+  }
+}
+
+// 한겨레 기사 페이지에서 데이터 추출해서 DB에 저장
+function getNewsHaniArticleData($, tag) {
+  var title = $(tag).find('span.title').text();
+  var content = $(tag).find('div.article-text div.text').text();
+  if (title.length > 0 && content.length > 0) {
+    // TODO: save to db
+    console.log(title);
+    //console.log(content);
+  }
+}
+
+// 중앙일보 이슈 페이지 URI 가져옴
+function getNewsJoinsIssueUri(issue) {
+  return NEWS_JOINS_ROOT + '/issue/' + issue;
+}
+
+// 조선일보 기사 검색 페이지 URI 가져옴
+function getChosunUri(searchStr) {
+  return encodeURI(NEWS_CHOSUN_ROOT + '/search/news.search?query=' + searchStr + '&orderby=default');
+}
+
+// 동아일보 기사 검색 페이지 URI 가져옴
+function getDongaUri(issueStr) {
+  return encodeURI(NEWS_DONGA_ROOT + '/Issue/' + issueStr);
+}
+
+function getHaniUri(issueNo) {
+  return encodeURI(NEWS_HANI_ROOT + '/arti/ISSUE/' + issueNo + '/list.html');
+}
+
+
 var newsJoinsIssueNo = 10357;
 var chosunSearchStr = '4대강사업';
 var dongaIssueStr = 'List_03000000000068';
+var haniIssueNo = 78;
+
+//c.queue({
+//  uri: getNewsJoinsIssueUri(newsJoinsIssueNo),
+//  timeout: 60000
+//});
+
+//c.queue([
+//  getChosunUri(chosunSearchStr) + '&pageno=1',
+//  getChosunUri(chosunSearchStr) + '&pageno=2',
+//  getChosunUri(chosunSearchStr) + '&pageno=3'
+//]);
+
+//c.queue({
+//  uri: getDongaUri(dongaIssueStr),
+//  timeout: 60000
+//});
 
 c.queue({
-  uri: getNewsJoinsIssueUri(newsJoinsIssueNo),
+  uri: getHaniUri(haniIssueNo),
   timeout: 60000
 });
-
-c.queue([
-  getChosunUri(chosunSearchStr) + '&pageno=1',
-  getChosunUri(chosunSearchStr) + '&pageno=2',
-  getChosunUri(chosunSearchStr) + '&pageno=3'
-]);
