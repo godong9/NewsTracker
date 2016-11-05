d9f47c368ae21909daf05110317199623802d0a8
Donghyun Go <godong9@gmail.com>
2016-11-05 14:15:42 +0900

    Update articles
    - Update model, controller

--- news-tracker/models/articles.js
+++ news-tracker/models/articles.js
@@ -4,19 +4,32 @@ var log4js = require('log4js');
 var logger = log4js.getLogger('ArticleModel');
 
 function ArticleModel() {
-  if (!(this instanceof ArticleModel)) {
-    return new ArticleModel();
-  }
+
 }
 
-ArticleModel.prototype.selectById = function (criteria, options, callback) {
+ArticleModel.selectById = function(criteria, options, callback) {
 
 };
 
-ArticleModel.prototype.insert = function (criteria, callback) {
+ArticleModel.select = function(criteria, options, callback) {
+  var sql = 'SELECT * FROM article WHERE issue_id=? ORDER BY write_at DESC LIMIT ? OFFSET ?;';
+  var values = [ criteria.issue_id, options.limit, options.offset ];
+  var query = mysql.format(sql, values);
+
+  pool.query(query, function (err, result) {
+    if (err) {
+      logger.error(err);
+    }
+    if (callback) {
+      callback(err, result);
+    }
+  });
+};
+
+ArticleModel.insert = function(criteria, callback) {
   var current_date = new Date();
-  var sql = 'INSERT INTO article (title, content, write_at, uri, company_id, created_at, updated_at) VALUES (?,?,?,?,?,?,?);';
-  var values = [ criteria.title, criteria.content, criteria.write_at, criteria.uri, criteria.company_id, current_date, current_date ];
+  var sql = 'INSERT INTO article (issue_id, title, content, write_at, uri, company_id, created_at, updated_at) VALUES (?,?,?,?,?,?,?,?);';
+  var values = [ criteria.issue_id, criteria.title, criteria.content, criteria.write_at, criteria.uri, criteria.company_id, current_date, current_date ];
   var query = mysql.format(sql, values);
 
   pool.query(query, function (err, result) {
